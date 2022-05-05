2dde7fd62320cb106bee9e1d3bbb804c5fcade2d
mahesh jamdade <maheshmn121@gmail.com>
2022-05-05 22:22:38 +0530

    remove unnecessary files

--- lib/constants/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// declare colors here
-///
-const Color primaryGreen = Color(0xff4c9648);
--- lib/constants/styles.dart
+++
@@ -1,3 +0,0 @@
-/// mention style constants to be used across different pages in you app here
-/// e.g borderRadius,textstyle,gradients etc
-///
