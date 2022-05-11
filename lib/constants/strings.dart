94c88af44634adf3dae41565d67c89d1ef6145c4
mahesh jamdade <maheshmn121@gmail.com>
2022-05-11 21:27:30 +0530

    fix typo

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,12 +3,12 @@
 const String appTitle = 'Pastelog';
 
 String hint =
-    '''Publish your logs to the cloud,\nAnd access them from anywhere via a unique link.
+    '''Publish your logs to the cloud,and access them from anywhere via a unique link.
 
       \nNo Sign in required!
       
       
-      \nNote: Do not paste any confidential info here as these logs will be publicly available for anyone with the link.
+      \nNote: Do not paste any confidential info here as these logs will be publicly available to anyone with the link.
       ''';
 
 String about =
