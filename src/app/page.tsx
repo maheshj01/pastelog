6557efc94c3d8282ebb4c3d8b4eaa31b47d70418
mahesh jamdade <maheshmn121@gmail.com>
2024-05-30 21:01:24 -0400

    render pastelog on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Button } from '@nextui-org/button';
 import { useEffect, useState } from 'react';
+import Pastelog from './_components/Pastelog';
 
 export default function Home() {
   const [isFirstVisit, setIsFirstVisit] = useState<boolean>(true);
-
   useEffect(() => {
-    const visited = localStorage.getItem('visited');
+    console.log("checking local storage for new user")
+    const visited = localStorage.getItem(`${process.env.NEXT_PUBLIC_NEW_USER_VISITED}`);
     if (visited) {
       setIsFirstVisit(false);
     } else {
@@ -14,15 +15,30 @@ export default function Home() {
     }
   }, []);
 
-  return (
-    <div className="relative min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400">
-      <div className="absolute inset-0 bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400 backdrop-blur-lg"></div>
-      <div className="relative z-10 max-w-2xl p-6 bg-white bg-opacity-80 rounded-lg shadow-lg">
+  if (isFirstVisit) {
+    return <Pastelog />
+  }
+  if (!isFirstVisit) {
+    return (
+      <div className="relative min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400">
+        <div className="absolute inset-0 bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400 backdrop-blur-lg"></div>
+        {/* <div className="relative z-10 max-w-2xl p-6 bg-white bg-opacity-80 rounded-lg shadow-lg">
 
+        </div> */}
+        <Button color="primary"
+          size='lg'
+          onClick={() => {
+            setIsFirstVisit(true);
+          }}
+          href="/pastelog"> Get Started </Button>
       </div>
-      <Button color="primary"
-        size='lg'
-        href="/pastelog"> Get Started </Button>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
     </div>
-  );
+  )
 }
+
