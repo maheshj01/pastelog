143adcd41977f16b9640d20de1fa8147a79999e8
mahesh jamdade <maheshmn121@gmail.com>
2024-06-08 14:05:52 -0400

    fixed overflow issue

--- src/app/_components/Home.tsx
+++ src/app/_components/Home.tsx
@@ -10,33 +10,31 @@ export default function Home() {
     const [showSideBar, setShowSideBar] = useState<boolean>(true);
 
     return (
-        <div className="flex">
-            {/* sidebar */}
-            {showSideBar && (
-                <IconButton
-                    className='absolute top-2 left-2 z-30'
-                    onClick={() => {
-                        setShowSideBar(!showSideBar);
-                    }}
-                    ariaLabel="Toggle sidebar"
-                >
-                    <ViewSidebarRoundedIcon />
-                </IconButton>
-            )}
+        <div className="flex h-screen">
+            {/* Sidebar toggle button */}
+            <IconButton
+                className={`fixed top-2 left-2 z-30 ${showSideBar ? '' : 'ml-0'}`}
+                onClick={() => setShowSideBar(!showSideBar)}
+                ariaLabel="Toggle sidebar"
+            >
+                <ViewSidebarRoundedIcon />
+            </IconButton>
 
-            {/* Sidebar content goes here */}
-            <div className={`bg-slate-200 dark:bg-gray-500 ${showSideBar ? 'w-64' : 'w-0'} transition-width duration-1000 pt-16 overflow-y-auto`}>
-                {/* Add your sidebar content here */}
-                {
-                    showSideBar && (
-
-                        <p>Sidbar content goes here</p>
-                    )
-                }
+            {/* Sidebar */}
+            <div
+                className={`fixed top-0 left-0 bottom-0 bg-slate-200 dark:bg-gray-500 overflow-y-auto transition-width duration-1000 ${showSideBar ? 'w-64' : 'w-0'
+                    }`}
+            >
+                {showSideBar && (
+                    <div className="pt-16">
+                        <p>Sidebar content goes here</p>
+                    </div>
+                )}
             </div>
 
-            <div className="grow">
-                <div className='flex flex-col'>
+            {/* Main content */}
+            <div className={`grow overflow-y-auto transition-all duration-800 ${showSideBar ? 'pl-64' : 'pl-0'}`}>
+                <div className="flex flex-col h-full">
                     <PSNavbar
                         sideBarIcon={!showSideBar}
                         onToggleSidebar={() => setShowSideBar(!showSideBar)}
