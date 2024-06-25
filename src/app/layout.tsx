0ca338882222d0ab0344752a1b729aca35aea141
mahesh jamdade <maheshmn121@gmail.com>
2024-06-24 23:04:20 -0400

    add metadata to welcome page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
-"use client";
-
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from './(main)/_components/ThemeProvider';
 import ToastProvider from "./(main)/_components/ToastProvider";
 import "./globals.css";
 import { Providers, SidebarProvider } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pastelog",
+  description: "Create Stunning Rich Text Logs/Notes with markdown Support and Code Highlighting and share it with the world.",
+
+};
 export default function RootLayout({
   children,
 }: Readonly<{
