59750f6eac9f8d7f0ec5afd0656adaf7161ccf06
mahesh jamdade <maheshmn121@gmail.com>
2024-06-26 09:34:34 -0400

    wrap  usesearchparams in Suspense

--- src/app/(main)/logs/page.tsx
+++ src/app/(main)/logs/page.tsx
@@ -1,19 +1,26 @@
 "use client";
+
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import Pastelog from "../_components/Pastelog";
 
-export default function LogsPage() {
+function LogsContent() {
     const searchParams = useSearchParams()
     const id = searchParams.get('id')
-    console.log(id);
     return (
         <div className={`grow`}>
             {/* Main content */}
             <div className="flex flex-col h-full">
-                <Pastelog
-                    id={id!}
-                />
+                <Pastelog id={id!} />
             </div>
         </div>
+    );
+}
+
+export default function LogsPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <LogsContent />
+        </Suspense>
     );
 }
