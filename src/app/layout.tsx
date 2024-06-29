1d0f94abec03dc42287d4c05498dda579a7db7a5
mahesh jamdade <maheshmn121@gmail.com>
2024-06-29 18:54:45 -0400

    update logo in tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.ico" />
+      </head>
       <body
         className={`${inter.className}`}>
         <Providers>
