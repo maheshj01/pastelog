636563a7fd18eaf48d50f63b0309b1c7bccb7c4f
mahesh jamdade <maheshmn121@gmail.com>
2024-10-14 22:59:36 -0400

    global shortcuts should not work with shift

--- src/app/(main)/_components/ShortCutWrapper.tsx
+++ src/app/(main)/_components/ShortCutWrapper.tsx
@@ -8,7 +8,7 @@ interface ShortcutWrapperProps {
 
 const ShortcutWrapper: React.FC<ShortcutWrapperProps> = ({ onShortCutClick, children }) => {
     const handleKeyPress = useCallback((event: KeyboardEvent) => {
-        if ((event.ctrlKey || event.metaKey) && (event.key == 'p' || event.key == 'n' || event.key == 's' || event.key == 'd')) {
+        if ((event.ctrlKey || event.metaKey) && (event.key == 'p' || event.key == 'n' || event.key == 's' || event.key == 'd') && (!event.shiftKey && !event.altKey)) {
             event.preventDefault();
             if (onShortCutClick) {
                 onShortCutClick(event.key);
