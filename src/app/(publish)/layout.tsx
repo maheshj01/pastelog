b0364079efc126b43ed4f68b3758105d5c164ffe
mahesh jamdade <maheshmn121@gmail.com>
2024-12-25 15:47:29 -0500

    fix publish broken

--- src/app/(publish)/layout.tsx
+++ src/app/(publish)/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { PSNavbarProvider } from "@/lib/Context/PSNavbarProvider";
 import PSNavbar from "../(main)/_components/PSNavbar";
 
 export default function PublishLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col">
-            <PSNavbar />
-            {children}
+            <PSNavbarProvider>
+                <PSNavbar />
+                {children}
+            </PSNavbarProvider>
         </div>
     );
 }
