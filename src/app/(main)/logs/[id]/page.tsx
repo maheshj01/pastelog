b45ced9a9be5f49096491c3b634851387895ed00
mahesh jamdade <maheshmn121@gmail.com>
2025-04-01 19:15:29 -0400

    format

--- src/app/(main)/logs/[id]/page.tsx
+++ src/app/(main)/logs/[id]/page.tsx
@@ -1,30 +1,48 @@
-import PreviewPage from '@/app/(main)/_components/PreviewPage';
-import { Metadata, ResolvingMetadata } from 'next';
-import LogService from '../../_services/logService';
+import PreviewPage from "@/app/(main)/_components/PreviewPage";
+import { Metadata, ResolvingMetadata } from "next";
+import LogService from "../../_services/logService";
 
 type Props = {
     params: {
         id: string;
     };
     searchParams: URLSearchParams;
-}
+};
 
 export async function generateMetadata(
     { params, searchParams }: Props,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
-    const id = params.id
+    const id = params.id;
     const log = await new LogService().importLog(id);
     const meta = {
         title: log?.title || "Pastelog",
-        description: log?.data || "PasteLog is a simple, fast, and powerful pastebin. It is powered by firebase in the backend. It allows you to publish your logs, and access them from anywhere and any device via a unique link.",
-    }
+        description:
+            log?.data ||
+            "PasteLog is a simple, fast, and powerful pastebin. It is powered by firebase in the backend. It allows you to publish your logs, and access them from anywhere and any device via a unique link.",
+        url: `https://pastelog.vercel.app/logs/${id}`,
+        openGraph: {
+            type: "website",
+            siteName: "Pastelog",
+            title: log?.title || "Pastelog",
+            description:
+                log.data ||
+                "PasteLog is a simple, fast, and powerful pastebin. It is powered by firebase in the backend. It allows you to publish your logs, and access them from anywhere and any device via a unique link.",
+            url: `https://pastelog.vercel.app/logs/${id}`,
+            images: [
+                {
+                    url: "/images/frame.png",
+                    width: 512,
+                    height: 512,
+                    alt: "Pastelog",
+                },
+            ],
+        },
+    };
     return meta;
 }
 
 export default function LogPage({ params }: { params: { id: string } }) {
     const { id } = params;
-    return <PreviewPage
-        logId={id}
-    />;
-};
+    return <PreviewPage logId={id} />;
+}
